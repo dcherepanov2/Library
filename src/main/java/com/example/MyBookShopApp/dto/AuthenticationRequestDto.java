5c262604757f37d3b76b1f3c1525c74a79056ca7
Danil Cherepanov <danilcherep7@gmail.com>
2023-01-20 16:43:55 +0700

    do working of aop-logger

--- src/main/java/com/example/MyBookShopApp/dto/AuthenticationRequestDto.java
+++ src/main/java/com/example/MyBookShopApp/dto/AuthenticationRequestDto.java
@@ -1,6 +1,6 @@
 package com.example.MyBookShopApp.dto;
 
-import io.swagger.v3.oas.annotations.media.Schema;
+//import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
@@ -9,15 +9,15 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 @Data
-@Schema(description = "AuthenticationRequestDto Сущность предоставляющая пользователя в запросе, при аутентификации пользователя в системе")
+//@Schema(description = "AuthenticationRequestDto Сущность предоставляющая пользователя в запросе, при аутентификации пользователя в системе")
 @Component
 public class AuthenticationRequestDto  implements AopDto{
-    @Schema(description = "Логин пользователя")
+//    @Schema(description = "Логин пользователя")
     @NotBlank(message = "Username should be between 6 and 20 character")
     @Size(min = 6, max = 20, message = "Username should be between 6 and 20 character")
     private String username;
 
-    @Schema(description = "Пароль пользователя")
+//    @Schema(description = "Пароль пользователя")
     @NotBlank(message = "Password should ne not null")
     @Size(min = 8, max = 30, message = "Password should be between 6 and 20 character")
     private String password;
