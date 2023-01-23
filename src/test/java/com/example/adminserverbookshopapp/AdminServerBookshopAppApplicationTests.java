5219516518ad33323b0b36e269e4962e471321e4
Danil Cherepanov <danilcherep7@gmail.com>
2023-01-23 21:38:11 +0700

    server admin

---
+++ src/test/java/com/example/adminserverbookshopapp/AdminServerBookshopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.adminserverbookshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerBookshopAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
